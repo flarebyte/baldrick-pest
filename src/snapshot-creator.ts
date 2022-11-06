210e44451f8f7b02f279abc497ac6969e406c909
Olivier Huin <olih@users.noreply.github.com>
2022-11-06 10:31:22 +0000

    snapshot only for string

--- src/snapshot-creator.ts
+++ src/snapshot-creator.ts
@@ -4,59 +4,40 @@ import { writeSnapshotFile } from './snapshot-io.js';
 import { Result, fail, succeed } from './railway.js';
 
 type SnapshotResult = Result<
-  string | object,
+  string,
   {
-    actual: string | object;
-    expected: string | object;
+    actual: string;
+    expected: string;
     message: string;
   }
 >;
 
 export const checkSnapshot = async (
   executeResult: StepExecuteResult & { status: 'success' },
-  snapshotFileName: string,
+  snapshotFileName: string
 ): Promise<SnapshotResult> => {
-  if (executeResult.value.context.expected === undefined) {
-    await writeSnapshotFile(
-      snapshotFileName,
-      executeResult.value.actual,
-
-    );
+  const expected = executeResult.value.context.expected;
+  if (expected === undefined) {
+    await writeSnapshotFile(snapshotFileName, executeResult.value.actual);
     return succeed(executeResult.value.actual);
   }
 
-  const isSameType =
-    (typeof executeResult.value.context.expected === 'string' &&
-      typeof executeResult.value.actual === 'string') ||
-    (typeof executeResult.value.context.expected === 'object' &&
-      typeof executeResult.value.actual === 'object');
-  if (isSameType) {
-    const diffString = diff(
-      executeResult.value.context.expected,
-      executeResult.value.actual
-    );
-    if (diffString === null) {
-      return succeed({
-        actual: executeResult.value.actual,
-        expected: executeResult.value.context.expected,
-        message: 'Comparison with a null value',
-      });
-    }
-    const hasNoDifference = diffString.includes(
-      'Compared values have no visual difference'
-    );
-    return hasNoDifference
-      ? succeed(executeResult.value.actual)
-      : fail({
-          actual: executeResult.value.actual,
-          expected: executeResult.value.context.expected,
-          message: diffString,
-        });
+  const diffString = diff(expected, executeResult.value.actual);
+  if (diffString === null) {
+    return fail({
+      actual: executeResult.value.actual,
+      expected,
+      message: 'Comparison with a null value',
+    });
   }
-
-  return fail({
-    actual: executeResult.value.actual,
-    expected: executeResult.value.context.expected,
-    message: 'Types for actual and expected are different',
-  });
+  const hasNoDifference = diffString.includes(
+    'Compared values have no visual difference'
+  );
+  return hasNoDifference
+    ? succeed(executeResult.value.actual)
+    : fail({
+        actual: executeResult.value.actual,
+        expected,
+        message: diffString,
+      });
 };
