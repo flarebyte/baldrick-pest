8bb626dcca68bacf43ba04ef812ca406e1f951c1
Olivier Huin <olih@users.noreply.github.com>
2022-11-09 09:14:18 +0000

    Adds name to usecase

--- src/model-enhancer.ts
+++ src/model-enhancer.ts
@@ -1,6 +1,13 @@
-import { PestModel } from "./pest-model.js";
+import { PestModel } from './pest-model.js';
 
 export const enhanceModel = (model: PestModel): PestModel => {
-    const {title, cases} = model;
-    return { title, cases}
-}
+  const { title, cases } = model;
+  for (const key of Object.keys(cases)) {
+    let caseObj = cases[key];
+    if (!caseObj) {
+      continue;
+    }
+    caseObj.name = key;
+  }
+  return { title, cases };
+};
